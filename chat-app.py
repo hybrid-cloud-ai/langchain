f82694e29502456aff8e70a6669e29a3abe1137d
Jack Wang <129803882+hybrid-cloud-ai@users.noreply.github.com>
2023-11-24 16:38:20 +0800

    Create chat-app.py

---
+++ chat-app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
+
+
+def main():
+    llm = ChatOpenAI(temperature=0)
+
+    st.set_page_config(
+        page_title="My Great ChatGPT",
+        page_icon="🤗"
+    )
+    st.header("My Great ChatGPT 🤗")
+
+    # chat history init
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            SystemMessage(content="You are a helpful assistant.")
+        ]
+
+    # monitor user input
+    if user_input := st.chat_input("Please input your question!"):
+        st.session_state.messages.append(HumanMessage(content=user_input))
+        with st.spinner("ChatGPT is typing ..."):
+            response = llm(st.session_state.messages)
+        st.session_state.messages.append(AIMessage(content=response.content))
+
+    # show chat
+    messages = st.session_state.get('messages', [])
+    for message in messages:
+        if isinstance(message, AIMessage):
+            with st.chat_message('assistant'):
+                st.markdown(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message('user'):
+                st.markdown(message.content)
+        else:  # isinstance(message, SystemMessage):
+            st.write(f"System message: {message.content}")
+
+
+if __name__ == '__main__':
+    main()
